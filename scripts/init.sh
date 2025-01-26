7f8371b9246cb416f275986d657d591df40d8ca2
WMartel2 <will.mart@hotmail.com>
2025-01-26 01:32:27 +0000

    T2

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# One time script to initialize the tools required for the repo
 echo 'Initializing tools for the repository...'
 
+# Configure Git
 echo 'Configuring Git...'
 git config --global user.name $GIT_USER_NAME
 git config --global user.email $GIT_USER_EMAIL
 echo 'Git configured!'
-
-#one-time init script for the repo
