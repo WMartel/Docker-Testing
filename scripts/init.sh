62cf12fb376c5aa2aa71cff1b3d8e0e0de03323b
WMartel <10606973+WMartel@users.noreply.github.com>
2025-01-26 02:50:39 +0000

    SignTest

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,4 +7,5 @@ echo 'Initializing tools required for the repository...'
 echo 'Configuring Git...'
 git config --global user.name $GIT_USER_NAME
 git config --global user.email $GIT_USER_EMAIL
+git config --global gpg.format ssh
 echo 'Git configured!'
