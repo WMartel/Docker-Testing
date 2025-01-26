5229a8e4905edf33e2fd9cc8e170dc3e3223d7eb
WMartel <10606973+WMartel@users.noreply.github.com>
2025-01-26 02:08:34 +0000

    asd

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Script to keep the tools required for the repo up to date
-#echo 'Updating tools required for the repository...'
+echo 'Updating tools required for the repository...'
 
 # Update Git
-#echo 'Updating Git...'
-#apt-get install git
-#echo 'Git updated!'
+echo 'Updating Git...'
+apt-get install git
+echo 'Git updated!'
