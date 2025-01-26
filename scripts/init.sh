ece3830127495bd69b0cfe488e1b5ad8ff414c69
WMartel <10606973+WMartel@users.noreply.github.com>
2025-01-26 02:55:22 +0000

    asdas

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,4 +8,6 @@ echo 'Configuring Git...'
 git config --global user.name $GIT_USER_NAME
 git config --global user.email $GIT_USER_EMAIL
 git config --global gpg.format ssh
+#git config --global user.signingkey /PATH/TO/.SSH/KEY.PUB
+git config --global commit.gpgsign true
 echo 'Git configured!'
